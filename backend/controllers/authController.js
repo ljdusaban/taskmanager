4b840a4cee8ef2cc7b259cfe7fdbb23e151d0bae
ljdusaban <leojohndusaban@gmail.com>
2026-04-03 14:44:31 +0000

    changed ip

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,7 +3,7 @@ const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
-//I changed this line.
+//I changed this line.aaa
 
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
