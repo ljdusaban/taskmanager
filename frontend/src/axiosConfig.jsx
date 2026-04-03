314bb1f75b3f105b5472fde92b459b41ed3f0b9e
ljdusaban <leojohndusaban@gmail.com>
2026-04-03 14:22:44 +0000

    update ip

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://13.210.58.232:5001', // live
+  baseURL: 'http://54.252.241.110:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
